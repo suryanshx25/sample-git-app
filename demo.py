fbd2e6f22ad954ba7b6e9a873c917fee6ce2a023
Suryansh Singh <suryanshx25@gmail.com>
2024-08-18 17:47:53 +0530

    change name of DS Course

--- demo.py
+++ demo.py
@@ -3,8 +3,7 @@ import streamlit as st
 st.title('CampusX')
 
 st.header('Courses offered')
-st.subheader('Data Science')
+st.subheader('Data Science and Machine Learning')
 st.subheader('Data Analysis')
-st.subheader('Data Engineering')
 st.subheader('SQL')
 st.subheader('ML')
